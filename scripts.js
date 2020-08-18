e43e8d3babde0e2c46665f38895a7b77c0c1d44d
asubedy <frexpe@pm.me>
2020-08-18 21:05:08 +0545

    Fixed a small '.' error

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function check(){
     const userInput = document.getElementById("passwordInput").value;
     let pw = String.fromCharCode(97, 77, 97, 90, 105, 110, 71); // What might this be? Well a search can come handy I guess.
     if(userInput==pw){
-        console,log('True answer');
+        console.log('True answer');
         document.location.href = userInput+".htm";
     }
     else{
