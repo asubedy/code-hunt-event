8f6a936643d077923bdb24e21dac988ac71cdfec
asubedy <frexpe@pm.me>
2020-08-18 21:00:05 +0545

    Added some more settings for mobile resposniveness

--- scripts.js
+++ scripts.js
@@ -2,17 +2,15 @@ function check(){
     const userInput = document.getElementById("passwordInput").value;
     let pw = String.fromCharCode(97, 77, 97, 90, 105, 110, 71); // What might this be? Well a search can come handy I guess.
     if(userInput==pw){
+        console,log('True answer');
         document.location.href = userInput+".htm";
     }
     else{
         console.log("false");
         const container = document.getElementById("input");
-        const tryAgainPara = document.createElement('p');
+        const tryAgainPara = document.getElementById('warning');
         tryAgainPara.innerText = "Try Again!";
         tryAgainPara.style.color = "rgb(243, 34, 34)";
         container.appendChild(tryAgainPara);
-        setTimeout(() => {
-            container.removeChild(tryAgainPara);
-        }, 4000);
     }    
 }
