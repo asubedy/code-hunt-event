c1d590f81f1f64423878ec001dfa5ebd2aab932c
asubedy <frexpe@pm.me>
2020-08-18 14:49:26 +0545

    added try again warning, reduced time for animation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 function check(){
     const userInput = document.getElementById("passwordInput").value;
     let pw = String.fromCharCode(97, 77, 97, 90, 105, 110, 71); // What might this be? I better search this.
-   // console.log(pw);
     if(userInput==pw){
         document.location.href = userInput+".htm";
     }
     else{
         console.log("false");
+        const container = document.getElementById("input");
+        const tryAgainPara = document.createElement('p');
+        tryAgainPara.innerText = "Try Again!";
+        tryAgainPara.style.color = "rgb(243, 34, 34)";
+        container.appendChild(tryAgainPara);
+        setTimeout(() => {
+            container.removeChild(tryAgainPara);
+        }, 4000);
     }    
 }
